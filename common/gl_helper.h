22c33ce83e918a8095f28d0d1ba668384cd43626
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2020-09-05 20:24:48 +0200

    small fix for mac maybe

--- common/gl_helper.h
+++ common/gl_helper.h
@@ -41,7 +41,8 @@
 #else
 
 /* On Linux, include the system's copy of glut.h, glext.h, and glx.h */
-#include <GL/glut.h>
+//#include <GL/glut.h>
+#include "GL/glut.h"
 #include <GL/glext.h>
 #include <GL/glx.h>
 
